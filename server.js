8e69757889ac78a8439286b3e44f9d20943e7f91
J Scott Rumptz <jscott@rumptz.tech>
2021-02-26 16:45:55 -0600

    parties complete

--- server.js
+++ server.js
@@ -41,6 +41,22 @@ app.get('/api/candidates', (req, res) => {
     });
 });
 
+// Get all parties
+app.get('/api/parties', (req,res) => {
+  const sql = `SELECT * FROM parties`;
+  const params = [];
+  db.all(sql, params, (err,rows) => {
+    if (err) {
+      res.status(500).json({error: err.message});
+    }
+
+    res.json({
+      message:'success',
+      data: rows
+    });
+  });
+});
+
 // Get single candidate
 app.get('/api/candidate/:id', (req, res) => {
     const sql = 
@@ -50,7 +66,7 @@ app.get('/api/candidate/:id', (req, res) => {
       LEFT JOIN parties 
       ON candidates.party_id = parties.id 
       WHERE candidates.id = ?`;
-      
+
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
       if (err) {
@@ -63,6 +79,23 @@ app.get('/api/candidate/:id', (req, res) => {
         data: row
       });
     });
+});
+
+// Get a single party
+app.get('/api/party/:id', (req,res) => {
+  const sql = `SELECT * FROM parties WHERE id =?`;
+  const params = [req.params.id];
+  db.get(sql, params, (err,row) => {
+    if(err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
 });
 
 // Delete a candidate
@@ -82,6 +115,24 @@ app.delete('/api/candidate/:id', (req, res) => {
     });
   });
 
+// Delete a party
+// We need to use normal function syntax for the db.run() callback instead 
+// of an arrow function, or else we'd lose the context of this.changes.
+app.delete('/api/party/:id', (req, res) => {
+  const sql = `DELETE FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+
+  // ES5 function, not arrow function, to use this
+  db.run(sql,params,function(err, result) {
+    if(err) {
+      res.status(400).json({ error: res.message });
+      return;
+    }
+
+    res.json({ message: 'successfully deleted', changes: this.changes });
+  });
+});
+
 // Create a candidate
 app.post('/api/candidate', ({ body }, res) => {
     const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
@@ -93,6 +144,7 @@ app.post('/api/candidate', ({ body }, res) => {
     const sql =  `INSERT INTO candidates (first_name, last_name, industry_connected) 
                   VALUES (?,?,?)`;
     const params = [body.first_name, body.last_name, body.industry_connected];
+
     // ES5 function, not arrow function, to use this
     db.run(sql, params, function(err, result) {
       if (err) {
@@ -106,6 +158,35 @@ app.post('/api/candidate', ({ body }, res) => {
         id: this.lastID
       });
     });
+});
+
+// Update a candidate
+app.put('/api/candidate/:id', (req, res) => {
+  // be extra sure that a party_id was provided
+  const errors = inputCheck(req.body, 'party_id');
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = 
+    `UPDATE candidates SET party_id = ?
+    WHERE id =?`;
+    const params = [req.body.party_id, req.params.id];
+
+  // ES5 function, not arrow function, to use this
+  db.run(sql, params, function(err, result) {
+    if(err) {
+      res.status(400).json({error: err.message});
+      return;
+    }
+
+    res.json({
+      message:'success',
+      data:req.body,
+      changes: this.changes
+    });
+  });
 });
 
 // Default response for any other request(Not Found) Catch all
